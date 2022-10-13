4b253b735c44fd6a86a594a0c6e5dbb705254b43
AxleTiger <bloodymountain@outlook.com>
2022-10-13 15:41:41 -0400

    update
    Renamed Strike command
    added unstrike command

---
+++ src/discord/commands/unstrike.js
@@ -0,0 +1,60 @@
+const { EmbedBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const config = require('../../../config.json')
+
+module.exports = {
+    name: 'unstrike',
+    description: 'unstrikes a staff member',
+    options: [
+        {
+            name: 'name',
+            description: 'discord Username',
+            type: 6,
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'reason for strike removal',
+            type: 3,
+            required: true
+        },
+        {
+            name: 'number',
+            description: 'the ammount of strikes they now have',
+            type: 3,
+            required: true
+        }
+      ],
+
+ 
+      execute: async (interaction, client) => {
+//
+//
+
+        const name = interaction.options.getUser("name")
+        const reason = interaction.options.getString("reason")
+        const number = interaction.options.getString("number")
+        const strikelog = {
+            title: 'Strike Log',
+            description: (`**${name}** had there strike removed for **${reason}**, they now have ${number} strikes!`),
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: (`Hyperbolic Staff Team`),
+            },
+        };
+        const discordlog = {
+            title: 'Staff Strikes',
+            description: (`${name} was unstriked!`),
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: (`Hyperbolic Staff Team`),
+            },
+        };
+        if ((await interaction.guild.members.fetch(interaction.user)).roles.cache.has(config.discord.commandRole)) {
+            await interaction.followUp({embeds: [discordlog] })
+            await client.channels.cache.get('1028779106453688361').send({embeds: [strikelog] })
+
+        } else {
+            await interaction.followUp({ content: 'You do not have permission to run this command.', ephemeral: true })
+        }
+      }
+    }
