b2cf8b15b99775deceff599a86b4de6c99384277
AxleTiger <bloodymountain@outlook.com>
2022-10-28 01:58:19 -0400

    oath2 Stuff

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 process.on('uncaughtException', function (err) {console.log(err.stack)})
-const webServer = require('./src/web/server.js'); 
-const app = require('./src/Application')
+const express = require('express');
+const webServer = require('./src/web/server.js');
+const { port } = require('./OAuth2config.json');
+const app = require('./src/Application');
+const chalk = require ('chalk');
+
 
 'use strict'
 process.title = 'Hypixel Discord Chat Bridge'
 
+
+
+const web = express();
+
+web.get('/', (request, response) => {
+  return response.sendFile('.src/oath/index.html', { root: '.' });
+});
+
+web.listen(port, () => console.log(chalk.bgGreen.black`App listening at http://localhost:${port}`));
+console.log(chalk.red.bold.underline`Warning!! OATH2 is currently in development for hyperbolic and is not reccomended to be used for general use due to the fact it uses your hoster's ip to connect your bot to discord, contact myself to get a copy of the bot not including AOTH2 connections!!`);
 
 app
   .register()   
@@ -13,4 +27,3 @@ app
   }).catch(err => {
     console.error(err) 
   })
-      
