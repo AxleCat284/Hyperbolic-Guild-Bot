71074502c257b94be95058b7895a3567330bffa9
AxleTiger <bloodymountain@outlook.com>
2022-10-31 23:58:23 -0400

    music config

---
+++ src/music/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    app: {
+        playing: 'Hyperbolic Music',
+        global: true,
+        guild: '859982166673588246'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: false,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
