229f01a18c311a3fe468fa9ba78845640afb1a40
AxleTiger <bloodymountain@outlook.com>
2022-11-15 20:53:34 -0500

    fixing perple's brain tax

--- src/discord/other/statsChannel.js
+++ src/discord/other/statsChannel.js
@@ -4,7 +4,7 @@ const config = require('../../../config.json');
 setInterval(async () => {
     try {
         const guild = await hypixel.getGuild("id", config.minecraft.guildID);
-        client.channels.cache.get(config.channels.guildMember).setName(`Guild Members: ${guild.members.length}/125`);
+        client.channels.cache.get(config.channels.guildMember).setName(`Guild Members: ${guild.members.length}/123`);
         client.channels.cache.get(config.channels.guildLevel).setName(`Guild Level: ${guild.level}`);      
     } catch (error) {
         console.log(error);
